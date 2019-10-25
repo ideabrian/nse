eedae679dee58f52eda9932033a61a640e3f26a9
Brian Ball <ideabrian@gmail.com>
2019-10-25 13:18:09 -0700

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <h1>Hello Team</h1>
+    <p>This is a Gatsby Site on Netlify</p>
+    <p>Get Ready!</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
