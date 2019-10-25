8156f375fdc33ed13fed07345274f0f36c97d1f1
Brian Ball <ideabrian@gmail.com>
2019-10-25 15:23:43 -0700

    Change footer and netlify.toml settings

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,9 +36,8 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          Built by
+          <a href="https://twitter.com/brianball">@brianball</a>
         </footer>
       </div>
     </>
