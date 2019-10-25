60f689d05b5375d1aa1070e83b86af777022d931
Brian Ball <ideabrian@gmail.com>
2019-10-25 13:40:33 -0700

    Add static icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Netlify Customers Need Awesome Support`,
+    description: `Netlify is a powertool. The support should be industrial strength.`,
+    author: `@brianball`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#00d6c5`,
       marginBottom: `1.45rem`,
     }}
   >
